f5060e398b81f422cb82956fd6d06ded3d25dec0
Sean Granier <seanlg624@gmail.com>
2021-04-03 10:19:17 -0500

    working on js practice 4

--- js-practice/js-practice-4/index.js
+++ js-practice/js-practice-4/index.js
@@ -10,18 +10,36 @@
     ☑️ create a string that contains your name
     ☑️ for loop over it, and print character to the console, one at a time
 */
+const user = "Sean";
 
+for (let i = 0; i < user.length; i++) {
+    console.log(user[i])
+}
 /*
     ☑️ create an array with the first names of everyone in your family
     ☑️ for loop over the array, and print out the names one at a time
     ☑️ now, change your loop to print out first and last name on each iteration!
-        (hint, your last name is the same for you whole family, right?)
+        (hint, your last name is the same for your whole family, right?)
 */
+// const family = ["alicia", "dawson", "fitz"];
+// const lastName = " gee";
+
+// for (members in family) {
+//     console.log(family[members] +  lastName);
+// }
+
 
 /*
     ☑️ copy the code you wrote above and paste it below this comment
     ☑️ now "refactor" this code into a "declarative" "for of" loop
 */
+const family = ["alicia", "dawson", "fitz"];
+const lastName = " gee";
+
+for (members of family) {
+    console.log(members + lastName);
+}
+
 
 /*
     ☑️ now, declare an index variable "i" (on its own) and set it to zero
@@ -29,7 +47,19 @@
     ☑️ ...because you need to increment i by one each time the loop runs (do this inside the loop body)
     ☑️ now console log i in the body, and make sure it's printing numbers 0-9
 */
+const i = 0;
 
+//REWATCH VIDEOS!!!!!
+
+while (true) {
+    if (i < 10) {
+        i++
+        break;
+    }
+    console.log(i);
+}
+
+}
 /*
     ☑️ ok take the while loop code from above and paste it below
     ☑️ now, change i to be an empty string
